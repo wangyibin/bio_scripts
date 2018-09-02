2929e877d49f0115aedb026ab2598f0e57023ceb
Yibin Wang <738339463@qq.com>
2018-09-02 16:07:44 +0800

    add single chrom caculate

--- myscript/getChrLength.py
+++ myscript/getChrLength.py
@@ -4,14 +4,18 @@
 ## Author:WangYibin
 """
 Usage:
-python %prog fasta > chromlength.txt
+python %prog fasta [chromosome_name] > chromlength.txt
+
+Return the length of each sequence in the fasta file.
+                                    Copyright:WangYibin
+
 """
 
 from Bio.SeqIO import parse
 from sys import argv
 
 
-def get_chr_length(infasta):
+def get_chr_length(infasta,chrn="all"):
     """
 
     """
@@ -19,15 +23,24 @@ def get_chr_length(infasta):
         fa = parse(f,'fasta')
 
         for record in fa:
-            print('%s\t%d'%(record.id,len(str(record.seq))))
+            if chrn == "all":
+                print('%s\t%d'%(record.id,len(str(record.seq))))
+            elif chrn == record.id:
+                print('%s\t%d'%(record.id,len(str(record.seq))))
+                break
+            else:
+                print('chromosome name error, please'
+                        ' input incorrect chromosome name of your fasta')
 
 
 if __name__ == "__main__":
     
-    if len(argv) != 2:
+    if len(argv) < 2 or len(argv) > 3:
         print('ERROR:')
         print(__doc__)
-    else:
-        infasta = argv[1]
+        sys.exit()
+    elif len(argv) == 3: 
+        chrn = argv[2]
 
-        get_chr_length(infasta)
+    infasta = argv[1]
+    get_chr_length(infasta,chrn)
